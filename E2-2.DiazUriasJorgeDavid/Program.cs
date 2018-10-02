9944f744b77a11bade9487ecab308e445a37b983
JDavidDiaz <43283432+JDavidDiaz@users.noreply.github.com>
2018-10-01 19:05:33 -0700

    E2-2.DiazUriasJorgeDavid

---
+++ E2-2.DiazUriasJorgeDavid/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace E2_2.DiazUriasJorgeDavid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero;
+            Console.WriteLine("Ingrese la cantidad maxima de numeros a sumar de la sucesion de fibonacci");
+            Numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nSucesion Con ciclo FOR:\n");
+            Fibonacci Sucesion = new Fibonacci();
+            Stopwatch Cronometro1 = Stopwatch.StartNew();
+            for (int i = 0; i < Numero; i++)
+            {
+                Console.WriteLine(Sucesion.Iteractivo(i));
+            }
+            Console.WriteLine("Tiempo en ejecucion: {0}", Cronometro1.Elapsed);
+            Console.WriteLine("\nSucesion Con Metodo de Recursividad:\n");
+            Recursividad Sucesion2 = new Recursividad();
+            Stopwatch Cronometro2 = Stopwatch.StartNew();
+            for (int j = 0; j < Numero; j++)
+            {
+                Console.WriteLine(Sucesion2.Recursivity(j));
+            }
+            Console.WriteLine("Tiempo en ejecucion: {0}", Cronometro2.Elapsed);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ E2-2.DiazUriasJorgeDavid/Recursividad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace E2_2.DiazUriasJorgeDavid
+{
+    class Recursividad
+    {
+        public int Recursivity(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+            else
+            {
+                return Recursivity(n - 1) + Recursivity(n - 2);
+            }
+        }
+    }
+}
